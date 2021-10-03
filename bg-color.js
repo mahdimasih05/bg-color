d1572a30e1421a2e6523c3b063bd303f0777bf01
Mahdi <mahdimasihipour@gmail.com>
2021-10-03 17:35:50 +0330

    manual select version

---
+++ bg-color.js
@@ -0,0 +1,35 @@
+function ChFunction() {
+  var value = document.getElementById("select").value;
+  switch (value) {
+    case "re":
+      document.body.style.backgroundColor = "red";
+      break;
+    case "gr":
+      document.body.style.backgroundColor = "green";
+      break;
+    case "bl":
+      document.body.style.backgroundColor = "blue";
+      break;
+    case "pi":
+      document.body.style.backgroundColor = "pink";
+      break;
+    case "or":
+      document.body.style.backgroundColor = "orange";
+      break;
+    case "ye":
+      document.body.style.backgroundColor = "yellow";
+      break;
+    case "pu":
+      document.body.style.backgroundColor = "purple";
+      break;
+    case "ge":
+      document.body.style.backgroundColor = "grey";
+      break;
+    case "bla":
+      document.body.style.backgroundColor = "black";
+      break;
+    case "wh":
+      document.body.style.backgroundColor = "white";
+      break;
+  }
+}
